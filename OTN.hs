55061630bf8295a08d519945856dd2aab96ea6c4
Gabor Greif <ggreif@gmail.com>
2017-12-02 23:11:29 +0100

    add blurb

--- OTN.hs
+++ OTN.hs
@@ -1,2 +1,8 @@
 module OTN where
 
+-- | OTN stands for Optical Transport Network.
+-- | Here we define seveal components (like NIMs,
+-- | i.e non-intrusive monitors, terminations,
+-- | cross-connection functions) according to the
+-- | ITU standard (https://www.itu.int/rec/T-REC-G.709/en).
+
