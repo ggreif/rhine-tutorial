eb7c4c72f38db4cd19ff5dbc391ccb677a302bf0
Gabor Greif <ggreif@gmail.com>
2017-12-02 23:20:45 +0100

    start defining OTU and ODU

--- OTN.hs
+++ OTN.hs
@@ -6,3 +6,14 @@ module OTN where
 -- | cross-connection functions) according to the
 -- | ITU standard (https://www.itu.int/rec/T-REC-G.709/en).
 
+-- | Simple-minded OTU, only containing a higher-order ODU
+--   note that we have no
+--   - sizing (bitrate: OTU1..4)
+--   - metadata
+--   yet.
+
+data OTU = OTU { ho :: ODU } deriving Show
+
+-- | Simple-minded ODU, only carrying a payload and some metadata
+
+data ODU = ODU { payload :: Int, sapi :: String, dapi :: String } deriving Show
