dac565f32c4ed60427f6952b2d249b10d7f5efae
Gabor Greif <ggreif@gmail.com>
2017-12-03 20:58:57 +0100

    mark up

--- OTN.hs
+++ OTN.hs
@@ -2,10 +2,13 @@
 
 module OTN where
 
+-- * Imports
+
 -- rhine
 import FRP.Rhine
 import FRP.Rhine.Clock.Realtime.Millisecond
 
+-- * Data types
 
 -- | OTN stands for Optical Transport Network.
 -- | Here we define seveal components (like NIMs,
@@ -29,6 +32,8 @@ data ODU = ODU { payload :: Int, sapi :: String, dapi :: String } deriving Show
 -- pull a clock out of nothing...
 type FrameClock = Millisecond 100
 
+-- * Various functional model elements
+
 -- | monitor - check whether the metadata is as expected
 monitor :: SyncSF IO FrameClock ODU ODU
 monitor = proc i@ODU{..} -> do
@@ -43,6 +48,8 @@ terminate = proc OTU{..} -> returnA -< ho
 assemble :: SyncSF IO FrameClock ODU OTU
 assemble = proc ho -> returnA -< OTU{..}
 
+-- ** Ports
+
 -- | a rudimentary input port (we could model a fiber too!)
 portIn :: SyncSignal IO FrameClock OTU
 portIn = proc _ -> returnA -< OTU{ ho = ODU{payload=42, sapi = "Berlin", dapi = "Köln"}}
@@ -51,12 +58,19 @@ portIn = proc _ -> returnA -< OTU{ ho = ODU{payload=42, sapi = "Berlin", dapi = 
 portOut :: SyncSF IO FrameClock OTU ()
 portOut = proc _ -> returnA -< ()
 
+-- ** Crossconnect
+
+-- TBD
+
+-- * Tests
 
 -- | Build a pipeline and provide a clock. Run the whole thing in IO.
 otnTest = flow $ pipeline @@ waitClock
   where pipeline = portIn >-> terminate >-> monitor
                >-> frameCount
                >-> assemble >-> portOut
+
+-- * Utilities
 
 frameCount :: SyncSF IO FrameClock a a
 frameCount = syncId &&& counter >>> arr fst
